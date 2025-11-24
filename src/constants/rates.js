86f5fe2fe4b91d107b359b139c70b6b80e70bbca
CIoudGuy <infinite4334@gmail.com>
2025-11-24 15:40:12 +0200

    Delete src directory

--- src/constants/rates.js
+++
@@ -1,8 +0,0 @@
-export const INPUT_LIMIT = 20_000_000_000;
-
-export const RATE_PRESETS = [
-  { id: "new", label: "New rate", value: 0.0038, hint: "Latest Roblox DevEx payout per Robux" },
-  { id: "old", label: "Old rate", value: 0.0035, hint: "Previous DevEx payout per Robux" },
-];
-
-export const DEFAULT_BASE_RATE = RATE_PRESETS[0].value;
--- src/utils/splits.js
+++
@@ -1,3 +0,0 @@
-const makeId = () => (crypto.randomUUID ? crypto.randomUUID() : Math.random().toString(36).slice(2));
-
-export const defaultSplit = () => ({ id: makeId(), name: "Collaborator", pct: 10 });
