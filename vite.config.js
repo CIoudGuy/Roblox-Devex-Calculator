a93d8e759986662d05816897130961b4c4e82001
CIoudGuy <infinite4334@gmail.com>
2025-11-24 15:41:37 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  // Use root base for custom domain https://devex-calculator.dev/
-  base: "/",
-});
